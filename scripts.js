d8c8b229e9decc7d475a823b56ea6e6cdcf5f530
yousef layeqi mehrabani <youosof@gmail.com>
2023-12-27 00:59:47 +0330

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    const hourDiv = document.querySelector('.hour');
+    const minuteDiv = document.querySelector('.minute');
+    const secondDiv = document.querySelector('.second');
+    const timeDiv = document.querySelector('.time');
+    
+
+    function setDate(){
+
+        const today = new Date();
+
+        const second = today.getSeconds();
+        const secondDeg = ((second/60) * 360)+360;
+        secondDiv.style.transform = `rotate(${secondDeg}deg)`;
+
+        const minute = today.getMinutes();
+        const minuteDeg = ((minute/60) * 360)+360;
+        minuteDiv.style.transform = `rotate(${minuteDeg}deg)`;
+
+        const hour = today.getHours();
+        const hourDeg = ((hour/12) * 360)+360;
+        hourDiv.style.transform = `rotate(${hourDeg}deg)`;
+
+        timeDiv.innerHTML = '<span>' + '<strong>' + hour + '</strong>' + ' : ' + minute + ' : ' + second + '</small>' + '</span>';
+    }
+    setInterval(setDate, 1000);
+}
